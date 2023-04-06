cfe57bdba05db2e3955140bcee7295b7f9f49086
Junhobu <bm1549@naver.com>
2023-04-07 01:07:52 +0900

    BottomNavigationView 하단탭 구현 완료

--- app/src/main/java/com/example/mecha_capstone_uv_sensor_app/MainActivity.java
+++ app/src/main/java/com/example/mecha_capstone_uv_sensor_app/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.mecha_capstone_uv_sensor_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    HomeFragment homeFragment;
+    InfoFragment infoFragment;
+    SettingFragment settingFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+        infoFragment = new InfoFragment();
+        settingFragment = new SettingFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
+
+        NavigationBarView navigationBarView = findViewById(R.id.bottom_navigationview);
+        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
+                        return true;
+                    case R.id.setting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, settingFragment).commit();
+                        return true;
+                    case R.id.info:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, infoFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+
 }
